f8edb1195642430e1f436d87de3e3cddf0c3b278
Pippin404 <Baconpippin@gmail.com>
2025-07-02 13:47:54 -0400

    fully setup react, cleaned up

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const commentRoutes = require('./server/routes/comment');
 app.use('/comment', commentRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}!!`);
 });
